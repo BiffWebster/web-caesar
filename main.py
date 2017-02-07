8c66948e99dc1cc2dd5399f87dba828c57476cb8
BiffWebster <biffwebster@gmail.com>
2017-02-06 18:43:09 -0600

    This should be the last time

--- main.py
+++ main.py
@@ -21,16 +21,16 @@ import cgi
 def build_page(textarea_content):
 	rot_label = "<label>Rotate by:</label>"
 	rotation_input = "<input type='number' name='rotation'/>"
-		
+
 	message_label = "<label>Type a message:</label>"
 	textarea = "<textarea name= 'message'>" + textarea_content + "</textarea>"
 	submit = "<input type='submit'/>"
-	form = ("<form method= 'post'>" + 
+	form = ("<form method= 'post'>" +
 		    rot_label + rotation_input + "<br>" +
 		    message_label + textarea + "<br>" +
 		    submit + "</form>")
 	header = "<h2>Web Caesar</h2>"
-    
+
 	return header + form
 
 
@@ -39,16 +39,16 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
 	    content = build_page("")
 	    self.response.write(content)
-		
+
     def post(self):
 	    message = self.request.get("message")
 	    rotation = int(self.request.get("rotation"))
 	    encrypted_message = caesar.encrypt(message, rotation)
-		escaped_message = cgi.escaped(encrypted_message)
+	    escaped_message = cgi.escaped(encrypted_message)
 	    content = build_page(escaped_message)
-		
+
 	    self.response.write(content)
-		
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
